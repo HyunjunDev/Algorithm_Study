582f7639ab07cb5d139df6615b9d3baffae27e99
DESKTOP-OHD6DA9\USER <a77653505@gmail.com>
2022-07-17 03:51:54 +0900

    first commit.

---
+++ CodeUp/1109_보안카드 접수증.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string name;
+	int age;
+	char code;
+	float key;
+	cin >> name >> age >> code >> key;
+	cout << name << endl
+		<< age << endl
+		<< code << endl
+		<< key;
+	return 0;
+}
